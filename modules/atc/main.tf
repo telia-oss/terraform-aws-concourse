cc20799ed2f69ec323463221fe640d22135c8579
itsdalmo <kristian@doingit.no>
2018-11-20 15:20:46 +0100

    Use aws-env to support arbitrary secrets passed to the ATC environment

--- modules/atc/main.tf
+++ modules/atc/main.tf
@@ -141,7 +141,8 @@ data "aws_iam_policy_document" "atc" {
     ]
 
     resources = [
-      "arn:aws:secretsmanager:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:secret:/concourse*",
+      "arn:aws:secretsmanager:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:secret:/concourse/*",
+      "arn:aws:secretsmanager:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:secret:/concourse-deployment/*",
     ]
   }
 }
