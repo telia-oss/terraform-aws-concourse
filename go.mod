0ca324a0f150f8e4dfa692c2b9686ec0aef87c29
itsdalmo <kristian@doingit.no>
2019-07-22 12:25:56 +0200

    build ami as part of e2e tests

--- go.mod
+++ go.mod
@@ -4,7 +4,10 @@ go 1.12
 
 require (
 	github.com/aws/aws-sdk-go v1.21.1
+	github.com/go-sql-driver/mysql v1.4.1 // indirect
+	github.com/google/uuid v1.1.1 // indirect
 	github.com/gruntwork-io/terratest v0.17.5
+	github.com/pquerna/otp v1.2.0 // indirect
 	github.com/stretchr/objx v0.2.0 // indirect
 	github.com/telia-oss/terraform-aws-asg v0.0.0-20190709201244-62a794675591
 	golang.org/x/sys v0.0.0-20190712062909-fae7ac547cb7 // indirect
--- test/concourse_test.go
+++ test/concourse_test.go
@@ -1,15 +1,22 @@
 package concourse_test
 
 import (
+	"flag"
 	"fmt"
+	"io/ioutil"
+	"strings"
 	"testing"
 
 	asg "github.com/telia-oss/terraform-aws-asg/test"
 	concourse "github.com/telia-oss/terraform-aws-concourse/test"
 
+	"github.com/gruntwork-io/terratest/modules/aws"
+	"github.com/gruntwork-io/terratest/modules/packer"
 	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 )
+
+var amiID = flag.String("ami-id", "", "Concourse AMI ID.")
 
 func TestDefaultExample(t *testing.T) {
 	tests := []struct {
@@ -34,6 +41,7 @@ func TestDefaultExample(t *testing.T) {
 						"terraform":   "True",
 						"environment": "dev",
 					},
+					UserData: readGoldenFile(t, "testdata/atc-cloud-config.golden.yml"),
 				},
 				WorkerAutoscaling: asg.Expectations{
 					MinSize:         1,
@@ -44,6 +52,7 @@ func TestDefaultExample(t *testing.T) {
 						"terraform":   "True",
 						"environment": "dev",
 					},
+					UserData: readGoldenFile(t, "testdata/worker-cloud-config.golden.yml"),
 				},
 			},
 		},
@@ -53,12 +62,28 @@ func TestDefaultExample(t *testing.T) {
 		tc := tc // Source: https://gist.github.com/posener/92a55c4cd441fc5e5e85f27bca008721
 		t.Run(tc.description, func(t *testing.T) {
 			t.Parallel()
+
+			amiID := *amiID
+			if amiID == "" {
+				amiID = packer.BuildArtifact(t, &packer.Options{
+					Template: "../packer/template.json",
+
+					Vars: map[string]string{
+						"template_version": "dev",
+					},
+
+					Only: "amazon-ebs",
+				})
+				defer aws.DeleteAmiAndAllSnapshots(t, tc.region, amiID)
+			}
+
 			options := &terraform.Options{
 				TerraformDir: tc.directory,
 
 				Vars: map[string]interface{}{
-					"name_prefix": tc.name,
-					"packer_ami":  packerAMI
+					// aws_db_subnet_group requires a lowercase name.
+					"name_prefix": strings.ToLower(tc.name),
+					"packer_ami":  amiID,
 					"region":      tc.region,
 				},
 
@@ -71,11 +96,19 @@ func TestDefaultExample(t *testing.T) {
 			terraform.InitAndApply(t, options)
 
 			concourse.RunTestSuite(t,
-				terraform.Output(t, options, "atc_autoscaling_group"),
-				terraform.Output(t, options, "worker_autoscaling_group"),
+				terraform.Output(t, options, "atc_asg_id"),
+				terraform.Output(t, options, "worker_asg_id"),
 				tc.region,
 				tc.expected,
 			)
 		})
 	}
+}
+
+func readGoldenFile(t *testing.T, path string) string {
+	f, err := ioutil.ReadFile(path)
+	if err != nil {
+		t.Fatalf("failed to read golden file: %s", path)
+	}
+	return string(f)
 }
